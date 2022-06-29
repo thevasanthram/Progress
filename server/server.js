b909ded321956e5390c560a081f4a4ae39efb6b5
thevasanthram <thevasanthram@github.com>
2022-06-29 20:14:56 +0530

    setQuestion Api

--- server/server.js
+++ server/server.js
@@ -21,12 +21,44 @@ app.post('/register', (req, res) => {});
 
 app.post('/login', (req, res) => {});
 
-app.get('/getquestions', (req, res) => {});
+app.get('/getallquestions', (req, res) => {});
 
-app.post('/setquestions', (req, res) => {});
+app.post('/setquestions', async (req, res) => {
+  try {
+    const question = req.body.question;
+    const correctAnswer = req.body.correctanswer;
+    const options = req.body.options;
+
+    // console.log('');
+    // console.log('Question: ', question);
+    // console.log('CorrectAnswer: ', correctAnswer);
+    // console.log('Options: ', options);
+    // console.log('');
+
+    const document = await Question.create({
+      question: question,
+      correctanswer: correctAnswer,
+      options: options,
+    });
+
+    return res.json({
+      status: 'success',
+      Question: document,
+    });
+  } catch (err) {
+    return res.json({
+      status: 'error',
+      error: err,
+    });
+  }
+});
 
 app.post('/result', (req, res) => {});
 
 app.get('/history', (req, res) => {});
 
 app.get('/logout', (req, res) => {});
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
