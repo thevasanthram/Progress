170d600df56a9a3d059a5826b63435a0ddabfcde
thevasanthram <thevasanthram@github.com>
2022-06-30 22:54:50 +0530

    basic front-end structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <h2>Front-end Under Development</h2>
+      <BrowserRouter>
+        <Route path='/' component={IndexPage} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/home' component={Home} />
+        <Route path='/adminpanel' component={AdminPanel} />
+        <Route path='/studentpanel' component={StudentPanel} />
+        <Route path='/quiz' component={Quiz} />
+        <Route path='/result' component={Result} />
+        <Route path='/history' component={History} />
+      </BrowserRouter>
     </div>
   );
 }
