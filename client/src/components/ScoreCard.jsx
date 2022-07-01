fe064564dc0972994fe49ab38c0bbd2e61e3fa42
thevasanthram <thevasanthram@github.com>
2022-07-02 00:50:39 +0530

    Score Card base

--- client/src/components/ScoreCard.jsx
+++ client/src/components/ScoreCard.jsx
@@ -1,10 +1,35 @@
 import './ScoreCard.css';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function ScoreCard() {
+  const [scorecard, setScoreCard] = useState([]);
+  useEffect(async () => {
+    const response = await fetch('http://localhost:5000/scorecard', {
+      method: 'GET',
+    });
+
+    const data = await response.json();
+    if (data.status == 'error') {
+      setErrorResponse(data.error);
+      setErrorResponseState(true);
+
+      setTimeout(() => {
+        setErrorResponseState(false);
+      }, 5000);
+    } else {
+      setScoreCard(data.data);
+    }
+  }, []);
   return (
     <div>
-      <h1>ScoreCard</h1>
+      <div class='scorecard'>
+        <h1>ScoreCard</h1>
+        <hr />
+        {}
+        <div class='singlecard'></div>
+      </div>
     </div>
   );
 }
