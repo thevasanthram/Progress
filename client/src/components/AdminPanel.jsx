35ac849134918c79957043d9a0ca9a7dd35c8341
thevasanthram <thevasanthram@github.com>
2022-07-01 18:10:58 +0530

    Admin Panel

--- client/src/components/AdminPanel.jsx
+++ client/src/components/AdminPanel.jsx
@@ -1,10 +1,20 @@
 import './AdminPanel.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function AdminPanel() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Admin Panel</h1>
+    <div class='AdminPanelContent'>
+      <h1 id='progress'>Progress!</h1>
+      <button class='buttonAdminPanel' onClick={() => navigate('/preparequiz')}>
+        Prepare Quiz
+      </button>
+      <br />
+      <br />
+      <button class='buttonAdminPanel' onClick={() => navigate('/scorecard')}>
+        Score Card
+      </button>
     </div>
   );
 }
