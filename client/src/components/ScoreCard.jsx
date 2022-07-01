104f1e620a9aa3deb4e738d1b00e5f0bcdcef814
thevasanthram <thevasanthram@github.com>
2022-07-02 02:07:58 +0530

    Score Card

--- client/src/components/ScoreCard.jsx
+++ client/src/components/ScoreCard.jsx
@@ -5,30 +5,59 @@ import { useEffect } from 'react';
 
 function ScoreCard() {
   const [scorecard, setScoreCard] = useState([]);
-  useEffect(async () => {
-    const response = await fetch('http://localhost:5000/scorecard', {
-      method: 'GET',
-    });
+  const [errorResponse, setErrorResponse] = useState('');
+  const [errorResponseState, setErrorResponseState] = useState(false);
 
-    const data = await response.json();
-    if (data.status == 'error') {
-      setErrorResponse(data.error);
-      setErrorResponseState(true);
-
-      setTimeout(() => {
-        setErrorResponseState(false);
-      }, 5000);
-    } else {
-      setScoreCard(data.data);
+  useEffect(() => {
+    async function scorecardapi() {
+      console.log('entering');
+      const response = await fetch('http://localhost:5000/scorecard', {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+      console.log('response: ', response);
+      const data = await response.json();
+      if (data.status == 'error') {
+        setErrorResponse(data.error);
+        setErrorResponseState(true);
+      } else {
+        setScoreCard(data.data);
+      }
     }
+
+    scorecardapi();
   }, []);
   return (
     <div>
       <div class='scorecard'>
         <h1>ScoreCard</h1>
         <hr />
-        {}
-        <div class='singlecard'></div>
+        <div class='scorecardinnerdivision'>
+          {scorecard.map((card) => {
+            return (
+              <div class='singlecard'>
+                <h4>Student Register Number: {card.studentRollNo}</h4>
+                <hr />
+                {card.quiz.map((singleQuiz) => {
+                  return (
+                    <div class='quizdetails'>
+                      <h5>Score: {singleQuiz.score}</h5>
+                      <h5>Time: {singleQuiz.time}</h5>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+
+          {errorResponseState && (
+            <div class='errorResponseScoreCard'>
+              <h3>{errorResponse}</h3>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
