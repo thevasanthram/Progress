d4e02dedf9a4b0c31817ce97700a6f0e24bd12a8
thevasanthram <thevasanthram@github.com>
2022-07-01 18:06:13 +0530

    Student Panel

--- client/src/components/StudentPanel.jsx
+++ client/src/components/StudentPanel.jsx
@@ -1,10 +1,20 @@
 import './StudentPanel.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function StudentPanel() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Student Panel</h1>
+    <div class='StudentPanelContent'>
+      <h1 id='progress'>Progress!</h1>
+      <button class='buttonStudentPanel' onClick={() => navigate('/quiz')}>
+        Take Test
+      </button>
+      <br />
+      <br />
+      <button class='buttonStudentPanel' onClick={() => navigate('/history')}>
+        History
+      </button> 
     </div>
   );
 }
